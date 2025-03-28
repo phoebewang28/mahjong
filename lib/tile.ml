aa253f1c45e55c5df717cb24d788407f9bc74ac3
Phoebe Wang <134721943+phoebewang28@users.noreply.github.com>
2025-03-27 23:00:49 -0400

    First Tile Implementation
    - Tiles_Arr: initialization, shuffle
    - Discarded: initialized
    - Other functions from basic_types

---
+++ lib/tile.ml
@@ -0,0 +1,94 @@
+type da_pai =
+  | Dong
+  | Nan
+  | Xi
+  | Bei
+  | Zhong
+  | Fa
+  | Bai
+
+type suit =
+  | Tong
+  | Wan
+  | Tiao
+  | DaPai of da_pai
+
+(* R.I: for tiles of da_pai, num is always set to 0 *)
+type tile = {
+  num : int;
+  tao : suit;
+}
+
+let get_num (t : tile) = t.num
+let get_tao (t : tile) = t.tao
+
+let curr_index = ref 0
+
+(**[init_tiles] initializes 136 tiles in a given order*)
+let init_tiles () = 
+  let tiles = Array.make 136 {num = 1; tao = Tong} in
+  let index = ref 0 in 
+
+  (*adds 4 copies of any tile*)
+  let add_tiles number suit = 
+    for _ = 1 to 4 do
+      tiles.(!index) <- {num = number; tao = suit};
+      incr index
+    done
+  in 
+  (*adds the 3 suited tiles with associated numbers*)
+  let add_by_suit suit =
+    for i = 1 to 9 do
+      add_tiles i suit
+    done
+  in 
+  add_by_suit Tong;
+  add_by_suit Wan;
+  add_by_suit Tiao;
+
+  (*adds the four winds*)
+  add_tiles 0 (DaPai Dong);
+  add_tiles 0 (DaPai Nan);
+  add_tiles 0 (DaPai Xi);
+  add_tiles 0 (DaPai Bei);
+
+  (*adds the three dragons*)
+  add_tiles 0 (DaPai Zhong);
+  add_tiles 0 (DaPai Fa);
+  add_tiles 0 (DaPai Bai);
+
+  tiles
+
+  (**[shuffle] shuffles the tiles, used at initialization*)
+let shuffle tiles =
+  let rec shuffle_helper n =
+    (*done shuffling*)
+    if n = 0 then tiles
+    else
+      let i = Random.int n in
+      let temp = tiles.(n) in
+      tiles.(n) <- tiles.(i);
+      tiles.(i) <- temp;
+      shuffle_helper (n - 1)
+  in
+  shuffle_helper 135
+
+
+let tiles_arr = shuffle (init_tiles ())
+
+let discarded = ref [{num = 3110; tao = Tong}]
+
+let string_to_tile str =
+  let t = Str.split (Str.regexp " ") str in
+  match List.hd t with
+  | "Tong" -> { num = int_of_string (List.hd (List.tl t)); tao = Tong }
+  | "Wan" -> { num = int_of_string (List.hd (List.tl t)); tao = Wan }
+  | "Tiao" -> { num = int_of_string (List.hd (List.tl t)); tao = Tiao }
+  | "Dong" -> { num = int_of_string (List.hd (List.tl t)); tao = DaPai Dong }
+  | "Nan" -> { num = int_of_string (List.hd (List.tl t)); tao = DaPai Nan }
+  | "Xi" -> { num = int_of_string (List.hd (List.tl t)); tao = DaPai Xi }
+  | "Bei" -> { num = int_of_string (List.hd (List.tl t)); tao = DaPai Bei }
+  | "Zhong" -> { num = int_of_string (List.hd (List.tl t)); tao = DaPai Zhong }
+  | "Fa" -> { num = int_of_string (List.hd (List.tl t)); tao = DaPai Fa }
+  | "Bai" -> { num = int_of_string (List.hd (List.tl t)); tao = DaPai Bai }
+  | _ -> { num = 0; tao = Tong }
---
+++ lib/tile.mli
@@ -0,0 +1,21 @@
+type tile
+(** Type of mahjong tile *)
+
+type suit
+(** Type of suit of mahjong itle *)
+
+val get_num : tile -> int
+(** [get_num t] returns number associated with tile [t] *)
+
+val get_tao : tile -> suit
+(** [get_tao t] returns tao associated with tile [t] *)
+
+val discarded : tile list ref
+(** [discarded] is a list collecting the discarded tiles. It is initialized with a random tile for now.
+[List.hd discarded] is the most recently discarded tile.*)
+
+val tiles_arr : tile array
+(** [tiles_arr] is an array containing the order of tiles at game play*)
+
+val string_to_tile : string -> tile
+(** [string_to_tile str] turns a string [str] into a tile*)
