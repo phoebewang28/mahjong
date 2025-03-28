a2d6fd8bfa9f1419e8963ea12d9e6225b41e05de
pw483 <pw483@cornell.edu>
2025-03-28 00:20:57 -0400

    Name + Made Hidden Open
    - Need hidden to deal

--- lib/player.ml
+++ lib/player.ml
@@ -13,9 +13,11 @@ let create name index =
     name;
     index;
     money = 1500;
-    hidden = Array.make 14 { num = 3110; tao = Tong };
+    hidden = Array.make 14 (make_tile { num = 3110; tao = Tong });
     exposed = [];
   }
 
 let get_index p = p.index
 let get_money p = p.money
+
+let get_hidden p = p.hidden
