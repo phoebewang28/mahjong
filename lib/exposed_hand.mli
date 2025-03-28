6dc82c89f2c2770a7ed6611c60bb610b42d13910
jw2773 <jw2773@cornell.edu>
2025-03-28 00:18:30 -0400

    Enhance documentation for exposed_hand functions with detailed examples

--- lib/exposed_hand.mli
+++ lib/exposed_hand.mli
@@ -7,26 +7,27 @@ type exposed_hand
 
 val chi : tile -> exposed_hand -> exposed_hand
 (** Given tile [t], [chi t] adds consecutive-number set to exposed hand. 
-   - Example: [t] has number of value 1, numbers of all tiles inset will be = {1,2,3}
-   - Precondition: Only called when legal chi set is created. [t] have must be SMALLEST number. *)
+   - Example: [t] is 1 Tong, numbers of all tiles inset will be = {1,2,3} while
+   the type will be "Tong" and the group will be "Shun".
+   - Precondition: Only called when legal chi set is created.*)
 
 val peng : tile -> exposed_hand -> exposed_hand
 (** Given tile [t], [peng t] adds a triplet of identical tiles to the exposed
    hand. 
-   - Example: [t] has number of value 1, numbers of all tiles inset will be
-   = {1,1,1} 
+   - Example: [t] is 1 Tong, numbers of all tiles inset will be
+   = {1,1,1} while the type will be "Tong" and the group will be "San".
    - Precondition: Only called when legal peng set is created.*)
 
 val ming_gang : tile -> exposed_hand -> exposed_hand
 (** Given tile [t], [ming_gang t] adds a quad of identical tiles to the exposed
    hand. 
-   - Example: [t] has number of value 1, numbers of all tiles inset will be
-   = {1,1,1,1} 
+   - Example: [t] is 1 Tong, numbers of all tiles inset will be
+   = {1,1,1,1} while the type will be "Tong" and the group will be "Si".
    - Precondition: Only called when legal peng set is created.*)
 
 val exposed_hand_to_string : exposed_hand -> string
 (** [exposed_hand_to_string h] returns a string representation of the exposed
     hand [h].
-    - Example: if [h] has a KeZi of 1 tong and a ShunZi starting with 2 tiao,
-      then the string returned is "".
+    - Example: if [h] has a KeZi of 1 Tong and a ShunZi starting with 2 Tiao,
+      then the string returned is "1 Tong KeZi, 2 Tiao ShunZi".
     - Precondition: [h] must be a valid exposed_hand. *)
